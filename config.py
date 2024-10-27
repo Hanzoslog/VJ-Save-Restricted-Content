adf0ef8f95c3fd6dc9c8b8fb3f5d4d5700890f6f
Hanzoslog <ketbenduc@gmail.com>
2024-10-27 16:13:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22836334"))
+API_ID = int(os.environ.get("API_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "072b12319aa045abd8710bd92eafbf4b")
+API_HASH = os.environ.get("API_HASH", "")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://ketbenduc:cXv6MNsgWtRZbLiJ@cluster0.cjyln.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DB_URI", "")
